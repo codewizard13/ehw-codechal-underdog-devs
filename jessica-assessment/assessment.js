264fd2b7ca1ad4a305ead94d5ecc39a3a1ba4b48
Eric L. Hepperle <codewizard13@gmail.com>
2022-11-11 10:33:22 -0600

    ADD jessica-assessment/ folder

---
+++ jessica-assessment/assessment.js
@@ -0,0 +1,13 @@
+// Assessment with Jessica McKellar @ UnderdogDevs for Cohort 4
+
+let val = ''
+
+let arr = ['apples', 'oranges', 'pears']
+
+val = arr
+
+
+
+// -------------------
+
+console.log(val)
