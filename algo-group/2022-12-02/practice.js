20f43030d64d416042a841bece392a63eaffef74
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-02 16:02:41 -0600

    UPDATE practice.js:  SCAFFOLD code

--- algo-group/2022-12-02/practice.js
+++ algo-group/2022-12-02/practice.js
@@ -1,1 +1,64 @@
 console.log("HI")
+
+
+// Two Sum
+
+// https://leetcode.com/problems/two-sum/
+
+
+// create dictionary to hold results?
+
+/*
+
+
+*/
+
+sampleSet = [
+  [[2, 7, 11, 15], 9],
+  [ [3,2,4], 6],
+]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+
+  const options = {}
+
+  console.table([...nums, target])
+
+  let index2 = 0
+
+  // Nested for loop
+  for (i=0; i<nums; i++) {
+
+    console.log("i:", i)
+
+    if () {
+
+    })
+
+  }
+
+  // return results
+
+}
+
+function testValues(inputSet) {
+
+  inputSet.forEach(set => {
+    twoSum(set)
+  })
+
+}
+
+testValues(sampleSet)
+
+
+/*
+
+FOLI's algorithm hashmap propblem
+
+*/
