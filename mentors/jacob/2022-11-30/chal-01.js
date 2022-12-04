dc04ff3d375b684edbb34d31b3b46dfbb92fbdb1
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-04 12:45:06 -0600

    UPDATE file header

--- mentors/jacob/2022-11-30/chal-01.js
+++ mentors/jacob/2022-11-30/chal-01.js
@@ -1,21 +1,5 @@
 /* 
-leetcode-decrypt-str-6.js
-
-https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
-
-1309. Decrypt String from Alphabet to Integer Mapping
-
-You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
-
-Characters ('a' to 'i') are represented by ('1' to '9') respectively.
-Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
-
-Return the string formed after mapping.
-
-
-This version fixes errors that were encountered in Leetcode and makes
-the corrections work with a local test harness. It incorporates the
-fixes learned regarding the "remainder" variable and join().
+MENTOR JACOB
 
 Eric Hepperle
 2022-11-29
