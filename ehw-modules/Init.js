9683aa4177832aa906faf5a08db7485a2825b91f
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-07 12:17:17 -0600

    MENTOR Jacob:  CHAL 2022-12-07:  FUNCTIONIZED last week's chal

--- ehw-modules/Init.js
+++ ehw-modules/Init.js
@@ -9,6 +9,23 @@ const fileBabyNames1880 = `${pathDocs}/baby_names_1880_short.txt`
 
 const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+const vowels = ["A", "E", "I", "O", "U"]
+
+const months = {
+  1: { fullName: "January", abbrev: "Jan" },
+  2: { fullName: "February", abbrev: "Feb" },
+  3: { fullName: "March", abbrev: "Mar" },
+  4: { fullName: "April", abbrev: "Apr" },
+  5: { fullName: "May", abbrev: "May" },
+  6: { fullName: "June", abbrev: "Jun" },
+  7: { fullName: "July", abbrev: "Jul" },
+  8: { fullName: "August", abbrev: "Aug" },
+  9: { fullName: "September", abbrev: "Sep" },
+  10: { fullName: "October", abbrev: "Oct" },
+  11: { fullName: "November", abbrev: "Nov" },
+  12: { fullName: "December", abbrev: "Dec" },
+}
+
 const msgStyles = {
   error:
     `color: red;` +
@@ -47,7 +64,7 @@ const init = {
     babyNames1880: fileBabyNames1880,
     babyNames2020: fileBabyNames2020
   },
-  abc,
+  abc, months,
 
   // slurpFile: (filename) => {
   //   console.log(filename)
