fa4a6d97388dcb91e39e93095192c1dc5991ea80
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-13 12:07:59 -0600

    MENTOR Greg:  HW: CREATE file

---
+++ mentors/greg/2022-12-12/hw-01.php
@@ -0,0 +1,179 @@
+<?php
+
+/*
+MENTOR Greg: HOMEWORK
+
+What are all of the words that both start with a "TH" and end with a "TH"?
+
+
+Eric Hepperle
+2022-12-12
+
+V1
+*/
+
+$memStart = memory_get_usage();
+
+// INCLUDE FUNCTIONS LIBRARY
+require_once(__DIR__ . '/../inc/functions.php');
+
+// FILE PATHS
+define('DOCS_PATH', __DIR__ . '/../../../docs/');
+define('SCRABBLE_FILE', DOCS_PATH . 'sowpods.txt');
+
+echo DOCS_PATH;
+// exit;
+
+
+
+
+/*
+ALGORITHM
+
+DEFINE babyNames file constant
+DEFINE scrabbleWords file contstant
+
+DEFINE matchedNames as empty hashmap
+
+SLURP babyNames file into array (babyNames)
+SLURP scrabbleWords file into array (scrabbleWords)
+
+LOOP through baby names
+
+  IF reverse of current baby name exists in scrabble words list
+
+    ADD baby name to matches array
+
+  END
+
+END
+
+RETURN matchedNames
+
+*/
+
+
+/**
+ * Primary controller function.
+ */
+function main($memStart)
+{
+
+  $matchedNames = [];
+
+  // CREATE ARRAYS FROM FILES
+  $babyNames2020 = fileToArray(BABY_NAMES_2020);
+  $scrabbleWords = fileToHashmap(SCRABBLE_FILE);
+
+  echo '<ul>';
+
+  // LOOP through babyNames2020 AND BUILD hashmap
+  for ($i = 0; $i < count($babyNames2020); $i++) {
+
+    $currentName = strtoupper($babyNames2020[$i]);
+
+    $reversedName = strReverse2($currentName);
+
+    echo "<li>";
+    echo "$currentName reversed is <span style='color:green'>$reversedName</span></br>";
+    // echo "Reveresed name exists in scrabble words? " . $scrabbleWords[$reversedName] . "<br>";
+
+    if (isset($scrabbleWords[$reversedName])) {
+      // if ( array_key_exists($reversedName, $scrabbleWords) ) {
+      // if ( in_array($reversedName, $scrabbleWords) ) {
+
+      echo "<h4>Name reversed [$reversedName] IS A KEY in \$scrabbleWords</h4>";
+    }
+
+
+    echo "</li>";
+  }
+  echo '</ul>';
+
+
+
+  print_r($matchedNames);
+
+
+  $peak = memory_get_peak_usage() / 1024 / 1024;
+
+  echo "Peak: {$peak}\n";
+
+  
+$memEnd = memory_get_usage();
+$memTotal = ($memEnd - $memStart) / 1024 / 1024 . PHP_EOL;;
+echo "Mem usage: {$memTotal}\n";
+} // END main
+
+
+
+
+
+// function reversed($inputStr)
+// {
+
+
+//   $remainder = $inputStr;
+
+//   $count = 0;
+
+//   while (strlen($remainder) > 0) {
+
+//     // echo "<h3>Run: $count</h3>";
+
+//     // get last char
+//     $lastChar = substr($remainder, -1);
+//     // echo "lastChar: $lastChar<br>";
+
+//     // set $remainder as remainder
+//     $newStrLastIndex = strlen($remainder) - 1;
+//     // echo "\$newStrLastIndex = $newStrLastIndex</br>";
+//     $remainder = substr($remainder, 0, $newStrLastIndex);
+
+//     // append last char to $newStr ??
+
+
+//     $count++;
+//   }
+
+//   return $newStr;
+// }
+
+
+// RUN program
+
+main($memStart);
+
+
+
+// This was the original strReverse2, but I renamed it
+function strReverse($string)
+{
+
+  $strLen = strlen($string);
+
+  $leftP = 0;
+  $rightP = $strLen;
+
+  $newStr = '';
+
+  for ($i = $strLen - 1; $i >= 0; $i--) {
+    // echo "$i: $string[$i]<br>";
+    $newStr .= $string[$i];
+  }
+
+  return $newStr;
+}
+
+
+
+// This version reverses a string in place without any temporary variables
+//   and reduces the memory usage by half (~29 MB)
+function strReverse2($string)
+{
+  for ($i = strlen($string) - 1, $j = 0; $j < $i; $i--, $j++) {
+    list($string[$j], $string[$i]) = array($string[$i], $string[$j]);
+  }
+
+  return $string;
+}
