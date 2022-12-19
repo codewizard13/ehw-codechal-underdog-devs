bc980b831a8835c2ea4b18001e6f482afb28a345
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-19 11:48:02 -0600

    MENTOR Greg:  ADD reportMemUsage() to functions.php

--- mentors/greg/inc/functions.php
+++ mentors/greg/inc/functions.php
@@ -50,12 +50,11 @@ function fileToHashmap($filename)
   $linesStr = file_get_contents($filename);
   $linesArr = explode("\n", $linesStr);
   $linesHash = [];
-  
+
   foreach ($linesArr as $key => $value) {
     // echo "$key => $value<br>";
 
     $linesHash[$value] = true;
-
   }
 
   return $linesHash;
@@ -112,4 +111,28 @@ function smallWordsList($words)
   }
 
   return $smallWordsArr;
+}
+
+
+
+
+/**
+ * Calculate and report memory usage of a block of code.
+ * 
+ * @usage: Call at bottom of a code black that has $memStart as arg.
+ * 
+ * @param: mixed $memStart
+ * @return: void
+ */
+function reportMemUsage($memStart)
+{
+
+  // END memory test and return results
+  $peak = memory_get_peak_usage() / 1024 / 1024;
+
+  echo "Peak: {$peak}\n";
+
+  $memEnd = memory_get_usage();
+  $memTotal = ($memEnd - $memStart) / 1024 / 1024 . PHP_EOL;;
+  echo "Mem usage: {$memTotal}\n";
 }
