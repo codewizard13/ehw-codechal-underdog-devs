11b5b5e59b39d37676773028715f8110d68d3b5f
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-20 07:26:54 -0600

    MENTOR Greg:  HW: ALGORITHM done

--- mentors/greg/2022-12-19/algorithm.php
+++ mentors/greg/2022-12-19/algorithm.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
-MENTOR Greg: HOMEWORK
+MENTOR Greg: HOMEWORK 2022-12-19
 
-Write a function that takes a string substring as an argument and returns
+Write a function that takes a string `substring` as an argument and returns
 an array of all of the words that contain that substring (the substring
 can appear anywhere in the word).
 
@@ -12,5 +12,84 @@ ALGORITHM ONLY
 Eric Hepperle
 2022-12-19
 
-V2
+V1
 */
+
+/*
+ALGORITHM
+
+SLURP scrabble file into array (scrabbleArray)
+
+DEFINE needle to search for as string (substring)
+TH
+
+RETURN array of words containing substring
+
+CREATE small words array to test with
+AA, THE
+
+
+// FUNCTION: getWordsContainingSubstring(needle)
+DEFINE matchedWords array
+
+LOOP over each word in scrabbleArray
+
+  SET temp var currentWord
+
+  SANITIZE word and needle (cleanValidNeedle(word, needle))
+  
+  IF sanitizedWord is valid
+  AND substring is found in currentWord (foundIn(sanitizedWord, needle))
+
+    ADD currentWord to matchedWords
+
+  END valid word check
+
+END
+
+RETURN matchedWords
+
+
+// FUNCTION: foundIn(word, needle)
+LOOP over each char in currentWord
+
+  SET temp var wordChar = currentWord[i]
+
+  IF wordChar same as first letter of needle then start testing
+
+    IF substring is found in current word
+    ADD currentWord to matchedWords array
+
+  END
+
+END
+
+RETURN
+
+
+// FUNCTION wordIsValid(word, needle)
+TRIM whitespace from start and end of word
+
+IF word is shorter than needle (ALSO accounts for is word is whitespace only)
+  return FALSE
+END
+
+RETURN TRUE
+
+
+
+
+
+*/
+
+
+
+$currentWord = trim($currentWord);
+
+    if (strlen($currentWord) < strlen($needle)) {
+      continue;
+    }
+
+    if (startsWith($currentWord, $needle) && endsWith($currentWord, $needle)) {
+      array_push($matches, $currentWord);
+    }
