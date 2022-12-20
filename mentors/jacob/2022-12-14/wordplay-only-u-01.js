0c1a990a9a34633d31595679b672b57fd633cbf3
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-20 10:12:14 -0600

    MENTOR Jacob:  HW 2022-12-14: MOVE ALGORITHM to own file. CLEANUP code

--- mentors/jacob/2022-12-14/wordplay-only-u-01.js
+++ mentors/jacob/2022-12-14/wordplay-only-u-01.js
@@ -1,7 +1,5 @@
 /*
-MENTOR Jacob:
-
-CHALLENGE:
+MENTOR Jacob: HOMEWORK: 2022-12-14
 
 What are all of the words that have only “U”s for vowels?
 
@@ -12,75 +10,19 @@ V1
 
 */
 
-/*
-ALGORITHM
-
-
-// FUNCTION: main
-DEFINE empty results array
-SLURP scrabble file into array (scrabbleWords)
-
-LOOP through each word
-
-  IF currentWord contains only U's for vowels
-
-    PUSH result on to results array
-
-  END
-
-END
-
-RETURN results
-
-// FUNCTION: hasOnlyU(word)
-// - Input word string
-// - Return boolean
-DECLARE array found
-
-LOOP  through each char in word
-
-  LOOP through each vowel
-
-    IF char is vowel
-
-      ADD char to found array
-
-      IF there are 2 elements in found array
-        RETURN false
-      END
-
-    END
-
-  END vowels loop
-    
-END word char loop
-
-// AFTER looping though word if found array has length 1
-IF found has only one element and that element is U
-
-  RETURN true
-
-ELSE
-
-  RETURN false
-
-END
+/* ALGORITHM IN OWN FILE */
 
 
 /*
 LEARNINGS / NOTES:
 
 - declarative vs imperative
-
-returns true if and only if there is at least one U and no other vowels
-
-HOMEWORK: Implement the above algorithm I wrote
+- returns true if and only ifthere is at least one U and no other vowels
+- HOMEWORK: Implement the above algorithm I wrote
 
 */
 
 const fs = require('fs')
-
-// let scrabbleWords = ["Albania", "Uganda", "Togo", "Zimbabwe", "Beekeeper", "Afghanistan"]
 
 const vowels = ["A", "E", "I", "O", "U"]
 
@@ -149,20 +91,6 @@ function hasOnlyU(word) {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /// FUNCTIONS ///
