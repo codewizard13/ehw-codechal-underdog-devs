d165bb0fc7012107e2b4135ace753a920e5a2e79
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-26 12:03:08 -0600

    MENTOR Luke:  HW 2022-12-26: CREATE folder

--- mentors/luke/2022-12-22/chal-01.js
+++ mentors/luke/2022-12-22/chal-01.js
@@ -1,12 +1,14 @@
 /* 
-MENTOR LUKE: HOMEWORK: 2022-12-15
-What are all of the words that have only “E”s for vowels and are at least 15 letters long?
-REFACTOR hasOnlyOneVowel() to take needle arg and return a boolean
+MENTOR LUKE: CHAL: 2022-12-22
+
+Write a function that takes a string prefix as an argument
+and returns an array of all of the words that start with that
+prefix (the prefix has to be at the beginning of the word).
 
 Eric Hepperle
-2022-12-15
+2022-12-22
 
-V2
+V1
 
 */
 
@@ -19,31 +21,99 @@ ALGORITHM:
 
 
 
+// FUNCTION: wordsStartWith(words, prefix)
+SLURP file into array (scrabbleWords)
+
+DEFINE matcheWords array as empty
+
+LOOP through scrabbleWords
+
+  IF currentWord starts with prefix
+   add it to matchedWords
+  END
+
+END
+
+RETURN matchedWords
+
+
+// FUNCTION: startsWith(word, prefix)
+LOOP through each char in word
+
+  SET temp var wordChar = word[i]
+
+  LOOP through each prefix char
+    
+    // if word[i] !== prefix[i]
+    IF wordChar NOT SAME as prefix char
+
+      RETURN FALSE
+
+    END
+ 
+  END
+
+END
+
+RETURN TRUE
+
 */
 
 
-const VOWELS = new Set(["A", "E", "I", "O", "U"])
+// SLURP file into array
+let scrabblePath = '../../../docs/sowpods.txt'
+// let scrabbleWords = filenameToLines(scrabblePath)
 
-/**
- * Main: The main function; controller.
- */
-function main() {
+let prefix = "CHE"
+
+let scrabbleWords = ["CHEERY", "CHERRY", "CAN"]
+// let scrabbleWords = ["CHE"]
+
+function wordsStartWith(words, prefix) {
 
   // DEFINE return array
   const matchedWords = []
 
-  // SLURP file into array
-  let scrabblePath = '../../../docs/sowpods.txt'
-  let scrabbleWords = filenameToLines(scrabblePath)
+  for (let i = 0; i < words.length; i++) {
 
-  // DO STUFF...
+    let currentWord = words[i]
+
+    if (startsWith(currentWord, prefix)) {
+      matchedWords.push(currentWord)
+    }
+
+  }
 
   // OUTPUT RESULTS
   console.log(`\nMATCHED WORDS\n`)
   console.table(matchedWords)
+}
+wordsStartWith(scrabbleWords, prefix)
+
+
+
+
+function startsWith(word, prefix) {
+
+  // LOOP through each char in word
+  for (i = 0; i < word.length; i++) {
+
+    let wordChar = word[i]
+
+    // ONLY loop until no more prefix chars
+    for (j=0; j < prefix.length; j++) {
+
+    if (word[i] !== prefix[i]) {
+      return false
+    }
+
+    }
+
+  }
+
+  return true
 
 }
-main()
 
 
 
