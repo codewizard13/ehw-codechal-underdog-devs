9ce5f3b78e1b331848c61c8a9b0c3f1453238115
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-29 12:36:58 -0600

    MENTOR Greg:  HW REORG: TIDY comments

--- inc/paths.php
+++ inc/paths.php
@@ -1,17 +1,15 @@
 <?php
 
 /*
-Filename: routes.php
-Purpose:  Define fully qualified pathnames to folders in this repot. Use these
+Filename: paths.php
+Purpose:  Define fully qualified pathnames to folders in this repo. Use these
 constants througout the app. Include this in functions.php.
 
 */
 
 // Define Paths and Routing
 define('REPO_PATH', dirname(__FILE__) . '/../');
-// echo "<h2>I'm in routes.php!</h2>";
 
-// include_once(view-header.php');
 define('MENTORS_PATH', REPO_PATH . 'mentors/');
 define('ERIC_PATH', REPO_PATH . 'eric-selected/');
 define('DOCS_PATH', REPO_PATH . 'docs/');
